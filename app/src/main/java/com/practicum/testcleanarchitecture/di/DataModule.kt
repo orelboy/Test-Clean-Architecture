10b054d87cae90f1c51be62b47c508423d171ae5
tsyplenkovdo <danil.ciplenkov@yandex.ru>
2024-11-18 03:03:26 +0300

    room

--- app/src/main/java/com/practicum/testcleanarchitecture/di/DataModule.kt
+++ app/src/main/java/com/practicum/testcleanarchitecture/di/DataModule.kt
@@ -48,6 +48,7 @@ val dataModule = module {
 
     single {
         Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db")
+            .fallbackToDestructiveMigration()
             .build()
     }
 
--- app/src/main/java/com/practicum/testcleanarchitecture/di/ViewModelModule.kt
+++ app/src/main/java/com/practicum/testcleanarchitecture/di/ViewModelModule.kt
@@ -4,6 +4,7 @@ import com.practicum.testcleanarchitecture.presentation.cast.MoviesCastViewModel
 import com.practicum.testcleanarchitecture.presentation.movies.MoviesSearchViewModel
 import com.practicum.testcleanarchitecture.presentation.details.PosterViewModel
 import com.practicum.testcleanarchitecture.presentation.details.AboutViewModel
+import com.practicum.testcleanarchitecture.presentation.history.HistoryViewModel
 import com.practicum.testcleanarchitecture.presentation.names.NamesViewModel
 import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
@@ -34,5 +35,7 @@ val viewModelModule = module {
     viewModel {
         NamesViewModel(androidContext(), get())
     }
+
+    viewModel { HistoryViewModel(context = androidContext(), historyInteractor = get()) }
 
 }
